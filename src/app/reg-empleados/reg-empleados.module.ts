f10f6d0c804066b0a2b902b827c91e0dbbb1fd48
Diego Zuluaga <diegoalejandrozuluagaecheverri@gmail.com>
2017-04-07 09:48:42 -0500

    correccion de proyecto

---
+++ src/app/reg-empleados/reg-empleados.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { RegEmpleadosRoutingModule } from './reg-empleados-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RegEmpleadosRoutingModule
+  ],
+  declarations: []
+})
+export class RegEmpleadosModule { }
